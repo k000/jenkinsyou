276f38ed2baec8f0a4a3afebd7b46adb98f83f50
k000 <kintore@mineo.jp>
2021-10-30 22:01:32 +0900

    test

---
+++ src/main/java/com/jenkinstest/domain/logic/CalcLogic.java
@@ -0,0 +1,18 @@
+package com.jenkinstest.domain.logic;
+
+public class CalcLogic {
+	
+	public CalcLogic() {
+	}
+	
+	/**
+	 * 足し算を行い結果を返します
+	 * @param a
+	 * @param b
+	 * @return 計算結果
+	 */
+	public int addNum(int a, int b) {
+		return a + b; 
+	}
+
+}
---
+++ src/test/java/com/jenkinstest/domain/logic/CalcLogicTest.java
@@ -0,0 +1,18 @@
+package com.jenkinstest.domain.logic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalcLogicTest {
+
+	@Test
+	void test() {
+		System.out.println("テストを開始します。");
+		CalcLogic logic = new CalcLogic();
+		int expect = 2;
+		int result = logic.addNum(1, 1);
+		assertEquals(result, expect);
+	}
+
+}
